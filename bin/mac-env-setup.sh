9ded2dac4e4fe353e8ed6cdb3150da40cd710ea0
George Cooper <george.cooper@egloo.com>
2020-05-26 00:38:29 -0700

    feat(chore): env setup script updates

--- bin/mac-env-setup.sh
+++ bin/mac-env-setup.sh
@@ -15,6 +15,65 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+USER_SHELL="other"
+INSTALL_SHELL_MODE="print"
+
+recordBash() {
+    USER_SHELL="bash"
+}
+
+recordZsh() {
+    USER_SHELL="zsh"
+}
+
+recordInstallShellAutoMode() {
+    INSTALL_SHELL_MODE="auto"
+}
+
+promptShellInstallMode() {
+    echo "Some tools require updates to shell startup scripts. Would you like the installer to add those for you or print out instructions?"
+    select installShellMode in "Auto" "Print"; do
+        case $installShellMode in
+            # Install Homebrew
+            Auto ) recordInstallShellAutoMode; break;;
+            Print ) break;;
+        esac
+    done
+}
+
+checkForHomebrew() {
+    which -s brew
+    if [[ $? != 0 ]] ; then
+        # Install Homebrew
+        promptForHomebrew
+    else
+        homebrewInstallNotice="Homebrew detected: Do you wish to update Homebrew? "
+        homebrewInstallNotice="${homebrewInstallNotice}Installed packages will not be upgraded.\n\n"
+        homebrewInstallNotice="${homebrewInstallNotice}Note: this installer assumes Homebrew is up-to-date."
+
+        # echo -e for interpretting newlines
+        echo -e $homebrewInstallNotice
+        select yn in "Yes" "No"; do
+            case $yn in
+                # Update Homebrew
+                Yes ) brew update; break;;
+                No ) break;;
+            esac
+        done        
+    fi
+}
+
+promptForHomebrew() {
+    echo "Do you wish to install Homebrew?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            # Install Homebrew
+            Yes ) installHomebrew; break;;
+            No ) break;;
+        esac
+    done
+}
+
 installHomebrew() {
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
@@ -25,16 +84,104 @@ installSqitch() {
 
 installAssumeRole() {
     brew install remind101/formulae/assume-role
+
+    case $INSTALL_SHELL_MODE in
+        auto ) setupAssumeRoleForShell;;
+        print ) printAssumeRoleForShell;;
+    esac
 }
 
-echo "Do you wish to install Homebrew?"
-select yn in "Yes" "No"; do
-    case $yn in
-        # Install Homebrew
-        Yes ) installHomebrew; break;;
-        No ) break;;
+setupAssumeRoleForShell() {
+    case $USER_SHELL in
+        bash ) setupAssumeRoleForBash;;
+        zsh ) setupAssumeRoleForZsh;;
+    esac
+}
+
+printAssumeRoleForShell() {
+    case $USER_SHELL in
+        bash ) printAssumeRoleForBash;;
+        zsh ) printAssumeRoleForZsh;;
+    esac
+}
+
+printAssumeRoleForBash() {
+    echo -e "Place the following in your ~/.bashrc:\n\nalias assume-role='function(){eval \$(command assume-role \$@);}'\n"
+}
+
+printAssumeRoleForZsh() {
+    echo -e "Place the following in your ~/.zshrc:\n\nalias assume-role='function(){eval \$(command assume-role \$@);}'\n"
+}
+
+setupAssumeRoleForBash() {
+    echo -e "\nalias assume-role='function(){eval \$(command assume-role \$@);}'\n" >> $HOME/.bashrc
+}
+
+setupAssumeRoleForZsh() {
+    echo -e "\nalias assume-role='function(){eval \$(command assume-role \$@);}'\n" >> $HOME/.zshrc
+}
+
+installPython() {
+    brew install python
+    brew install pyenv
+
+    case $INSTALL_SHELL_MODE in
+        auto ) setupPyenvForShell;;
+        print ) printPyenvForShell;;
+    esac
+
+    # TODO check if we need to set the shell startup script first
+    pyenv install 3.8.2
+}
+
+setupPyenvForShell() {
+    case $USER_SHELL in
+        bash ) setupPyenvForBash;;
+        zsh ) setupPyenvForZsh;;
+    esac
+}
+
+printPyenvForShell() {
+    case $USER_SHELL in
+        bash ) printPyenvForBash;;
+        zsh ) printPyenvForZsh;;
+    esac
+}
+
+printPyenvForBash() {
+    echo -e 'Place the following in your ~/.bashrc:\n\neval "$(pyenv init -)"\n'
+}
+
+printPyenvForZsh() {
+    echo -e 'Place the following in your ~/.zshrc:\n\neval "$(pyenv init -)"\n'
+}
+
+setupPyenvForBash() {
+    echo -e '\neval "$(pyenv init -)"\n' >> $HOME/.bashrc
+}
+
+setupPyenvForZsh() {
+    echo -e '\neval "$(pyenv init -)"\n' >> $HOME/.zshrc
+}
+
+echo "Which shell do you generally use?"
+select userShell in "Bash" "Zsh" "Other"; do
+    case $userShell in
+        # Record shell choice
+        Bash ) recordBash; break;;
+        Zsh ) recordZsh; break;;
+        Other ) break;;
     esac
 done
+
+# If shell is supported, offer auto shell startup injections or print required inits
+case $USER_SHELL in
+    bash ) promptShellInstallMode;;
+    zsh ) promptShellInstallMode;;
+esac
+
+# Check for or install Homebrew
+checkForHomebrew
 
 # Install Sqitch
 echo "Do you wish to install Sqitch?"
@@ -58,3 +205,16 @@ select yn in "Yes" "No"; do
         No ) break;;
     esac
 done
+
+
+# Install python3 and pyenv
+echo "Do you wish to install python3 and pyenv?"
+select yn in "Yes" "No"; do
+    case $yn in
+        # Install assume-role
+        Yes ) installPython; break;;
+        No ) break;;
+    esac
+done
+
+echo "Environment setup complete."
